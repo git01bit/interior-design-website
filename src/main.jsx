c3e6562a142cdce19516c8dd13629c3936674ecc
git01bit <majidpour018@gmail.com>
2025-08-30 12:19:41 +0330

    feat(about-us): add about us page

--- src/main.jsx
+++ src/main.jsx
@@ -4,6 +4,7 @@ import { createRoot } from "react-dom/client";
 import "./index.css";
 import App from "./App.jsx";
 import ContactUs from "./Components/ContactUs.jsx";
+import AboutUs from "./Components/AboutUs.jsx";
 
 import { createBrowserRouter, RouterProvider } from "react-router";
 
@@ -15,6 +16,10 @@ const router = createBrowserRouter([
   {
     path: "/interior-design-website/contact-us",
     element: <ContactUs />,
+  },
+  {
+    path: "/interior-design-website/about-us",
+    element: <AboutUs />,
   },
 ]);
 
